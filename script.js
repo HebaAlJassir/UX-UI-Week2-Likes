463f8a7c998c9bb46587e99ca9f7f3d828ad3f7a
Heba <bon3bona@gmail.com>
2022-06-03 17:57:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var clicks1 = 0;
+
+function onClick1() {
+  clicks1 += 1;
+  document.getElementById("clicks1").innerHTML = clicks1;
+};
+
+var clicks2 = 0;
+
+function onClick2() {
+  clicks2 += 1;
+  document.getElementById("clicks2").innerHTML = clicks2;
+};
+
+var clicks3 = 0;
+
+function onClick3() {
+  clicks3 += 1;
+  document.getElementById("clicks3").innerHTML = clicks3;
+};
